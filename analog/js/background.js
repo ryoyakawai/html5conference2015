9d99fe8209210ee641302a6cd28f293c61648bb3
Ryoya KAWAI <ryoya.kawai@gmail.com>
2015-03-14 13:49:22 +0900

    Add samples

--- analog/js/background.js
+++ analog/js/background.js
@@ -1,6 +1,9 @@
 chrome.app.runtime.onLaunched.addListener(function() {
-  chrome.app.window.create('index.html', {
-    width: 785,
-    height: 660
+  chrome.app.window.create('build.html', {
+      "bounds" : {
+          width: 785,
+          height: 660
+      },
+      "resizable": false
   });
 });
