02e873e05610ea3c4bbb5f313d3208a35ab860f2
ryoyakawai <ryoya.kawai@gmail.com>
2015-10-23 12:42:36 +0900

    Added: Offline feature

---
+++ analog/sw.js
@@ -0,0 +1,53 @@
+/* sw.js */
+importScripts('serviceworker-cache-polyfill.js');
+var CACHE_NAME = 'html5conference2015-analog-cache';
+var urlsToCache = [
+    './index.html',
+    './bower_components/x-webmidi/x-webmidirequestaccess.html',
+    './components/polymer_analyser.html',
+    './bower_components/webaudio-controls/lib/webaudio-controls.min.html',
+    './css/osc2-00.css',
+    './sw.js',
+    './register_sw.js',
+    './js/osc2-00.js'
+];
+
+self.addEventListener('install', function(event) {
+  event.waitUntil(
+      caches.open(CACHE_NAME)
+          .then(function(cache) {
+              //console.log('Opened cache');
+              return cache.addAll(urlsToCache);
+          })
+  );
+});
+
+self.addEventListener('fetch', function(event) {
+  event.respondWith(
+      caches.match(event.request)
+          .then(function(response) {
+              if (response) {
+                  //console.log("[return cache] ", (response.url).split("/").pop());
+                  return response;
+              }
+              var fetchRequest = event.request.clone();
+
+              return fetch(fetchRequest)
+                  .then(function(response) {
+                      if (!response || response.status !== 200 || response.type !== 'basic') {
+                          return response;
+                      }
+                      
+                      var responseToCache = response.clone();
+
+                      caches.open(CACHE_NAME)
+                          .then(function(cache) {
+                              cache.put(event.request, responseToCache);
+                          });
+                      
+                      return response;
+                  });
+          })
+  );
+});
+
